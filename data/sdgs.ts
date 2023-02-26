8a8843647350c8480646c4a7f566c3fe29012474
Joonas Nuutinen <joonas.nuutinen@columbiaroad.com>
2023-02-26 17:02:21 +0200

    Add sdg data

---
+++ data/sdgs.ts
@@ -0,0 +1,77 @@
+interface SDG {
+  id: string
+  name: string
+}
+
+const sdgs = [
+  {
+    id: 'poverty',
+    name: 'No poverty'
+  },
+  {
+    id: 'hunger',
+    name: 'Zero hunger'
+  },
+  {
+    id: 'health',
+    name: 'Good health and well-being'
+  },
+  {
+    id: 'education',
+    name: 'Quality education'
+  },
+  {
+    id: 'gender',
+    name: 'Gender equality'
+  },
+  {
+    id: 'water',
+    name: 'Clean water and sanitation'
+  },
+  {
+    id: 'energy',
+    name: 'Affordable and clean energy'
+  },
+  {
+    id: 'work',
+    name: 'Decent work and economic growth'
+  },
+  {
+    id: 'industry',
+    name: 'Industry, innovation and infrastructure'
+  },
+  {
+    id: 'inequalities',
+    name: 'Reduced inequalities'
+  },
+  {
+    id: 'cities',
+    name: 'Sustainable cities and communities'
+  },
+  {
+    id: 'responsible',
+    name: 'Responsible consumption and production'
+  },
+  {
+    id: 'climate',
+    name: 'Climate action'
+  },
+  {
+    id: 'water',
+    name: 'Life below water'
+  },
+  {
+    id: 'land',
+    name: 'Life on land'
+  },
+  {
+    id: 'peace',
+    name: 'Peace, justice and strong institutions'
+  },
+  {
+    id: 'partnership',
+    name: 'Partnership for the goals'
+  },
+]
+
+export default sdgs
